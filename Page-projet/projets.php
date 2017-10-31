f7b8b3c1702df8fde696b16c414af3b2abb6316d
KamiGaara <33122547+KamiGaara@users.noreply.github.com>
2017-10-31 16:09:01 +0100

    Add files via upload

---
+++ Page-projet/projets.php
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+    <projets>
+        <projet nom="Lecture et commentaires de code" lien="http://adrient.e2simplon.fr/lecture/" ShowOnPersonalSite="false"></projet>
+        <projet nom="CV de Inori " lien="http://adrient.e2simplon.fr/cv/" ShowOnPersonalSite="true"></projet>
+        <projet nom="Explorateur de projets" lien="http://adrient.e2simplon.fr/projects-watcher/" ShowOnPersonalSite="true"></projet>
+    </projets>
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: text/xml');
+?>
